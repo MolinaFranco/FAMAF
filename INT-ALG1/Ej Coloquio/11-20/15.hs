ee7c6615bb8b17714ad065c2b4549b070c5da98e
MolinaFranco <franco13mm@gmail.com>
2021-06-22 13:47:49 -0300

    Algorit1

---
+++ INT-ALG1/Ej Coloquio/11-20/15.hs
@@ -0,0 +1,11 @@
+# Ley de absorcion
+
+_p ^ (p v q)_ ≡ p
+{regla dorada p:=p, q:=(p v q)}
+(p v q) ≡ p v (p v q)
+{Asociatividad disyunci ́on}
+(p v q) ≡ _(p v p)_ v q
+{Idempotencia disyuncion}
+(p v q) ≡ (p v q)
+
+
